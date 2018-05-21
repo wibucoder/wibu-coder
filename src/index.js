b51b9743a1961704e4b8ca734ae615dc49328f13
okuta <oktavanhowen@gmail.com>
2018-05-21 23:30:27 +0700

    learn components react

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const tanggalHariIni = () => {
-    const date = new Date();
-    return date.toDateString();
-}
 
-const greeting = <h1>Halo Wibu Tanggal hari ini : {tanggalHariIni()}</h1>
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
